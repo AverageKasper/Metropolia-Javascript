65ef6fa32eea830ec43861f7d5327b236f3f071b
AverageKasper <averagekasper@gmail.com>
2024-11-07 10:30:12 +0200

    Task 2 done

--- module_1/task03.js
+++ module_1/task03.js
@@ -4,12 +4,12 @@ const num1 = prompt('Enter the first number');
 const num2 = prompt('Enter the second number');
 const num3 = prompt('Enter the third number');
 
-const num1int= parseInt(num1);
-const num2int= parseInt(num2);
-const num3int= parseInt(num3);
+const num1int= parseFloat(num1);
+const num2int= parseFloat(num2);
+const num3int= parseFloat(num3);
 
 const sum = num1int + num2int + num3int;
 const product = num1int * num2int * num3int;
 const average = sum / 3;
-console.log(sum, product, average);
+
 document.getElementById("math").innerHTML = `The sum is ${sum}, product is ${product} and average is ${average}`;
