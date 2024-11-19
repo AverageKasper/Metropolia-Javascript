41796256d740f390b53d1bee8dfcfa975a652491
AverageKasper <averagekasper@gmail.com>
2024-11-19 10:26:33 +0200

    task 11 done

--- module_3/t10/10.js
+++ module_3/t10/10.js
@@ -2,3 +2,12 @@
 
 const p = document.getElementById('target');
 const form = document.getElementById('source');
+
+const first_name = document.querySelector('input[name="firstname"]');
+const last_name = document.querySelector('input[name="lastname"]');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let full_name = `You are ${first_name.value} ${last_name.value}`;
+    p.innerText = full_name;
+});
